c4099427338836d543db8ff957fed5f6e854c3b8
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-06-26 16:54:31 +0200

    :sparkles: Display all applications

---
+++ src/Application/index.js
@@ -0,0 +1,18 @@
+import { h, Component } from 'preact';
+
+export class Application extends Component {
+  constructor() {
+    super();
+    this.state = {
+      active: false,
+    };
+  }
+
+  render({ name, href }) {
+    return (
+      <div>
+        <span>{name} ({href})</span>
+      </div>
+    );
+  }
+}
--- src/ApplicationLauncher/index.js
+++ src/ApplicationLauncher/index.js
@@ -1,9 +1,14 @@
 import { h } from 'preact';
+import { fetchApplications } from '../fetchApplications';
+import { Application } from '../Application';
 
-export function ApplicationLauncher() {
+export function ApplicationLauncher({ url }) {
+  const applications = fetchApplications(url);
+
   return (
     <div>
       <span>Hello JSX!</span>
+      {applications.map(application => <Application {...application} />)}
     </div>
   );
 }
