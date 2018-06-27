ce6c9cc2d588bf8271aca6c919803f26637fb023
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-06-27 15:44:59 +0200

    :clown_face: Add application mock data and :white_check_mark: updating tests

--- src/__tests__/fetchApplications.test.js
+++ src/__tests__/fetchApplications.test.js
@@ -1,8 +1,11 @@
 import { fetchApplications } from '../fetchApplications';
 
 describe('fetch applications', () => {
-  it('should fetch applications in the correct format', () => {
-    const applications = fetchApplications('my.domain'); // TODO change me
-    applications.every(application => expect(application).toHaveProperty('name') && expect(application).toHaveProperty('href') && expect(application).toHaveProperty('img'));
+  it('should fetch applications in the correct format', async () => {
+    const applications = await fetchApplications('my.domain'); // TODO change me
+    applications.every(application => expect(application).toHaveProperty('id')
+      && expect(application).toHaveProperty('name')
+      && expect(application).toHaveProperty('href')
+      && expect(application).toHaveProperty('img'));
   });
 });
--- src/fetchApplications.js
+++ src/fetchApplications.js
@@ -7,16 +7,22 @@ export async function fetchApplications(domain) {
 
   return Promise.resolve([
     {
+      id: 0,
+      name: 'Home',
+      href: 'https://mywebportal.michelin-solutions.com/home',
+      img: 'https://mywebportal.michelin-solutions.com/msn-applications/5da990a45725cd07b890f6b62a7bbb66.png',
+    },
+    {
       id: 1,
       name: 'MyInspection',
       href: 'https://mywebportal.michelin-solutions.com/my-inspection',
-      img: '',
+      img: 'https://mywebportal.michelin-solutions.com/msn-applications/595847694218e668013f7dc4339790f2.png',
     },
     {
       id: 2,
       name: 'MyTraining',
       href: 'https://mywebportal.michelin-solutions.com/my-training',
-      img: '',
+      img: 'https://mywebportal.michelin-solutions.com/msn-applications/ae001618ead2779cbb3f7e5c44b6ec0f.png',
     },
   ]);
 }
