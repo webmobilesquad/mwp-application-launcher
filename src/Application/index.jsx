ebd8ea8ede5c0485cccb236050ff4ca632050f01
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-07-02 16:50:15 +0200

    :sparkles: Use application active state

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,15 +1,29 @@
 import { Component } from 'preact';
+import cx from 'classnames';
 import style from './style.module.scss';
+import isApplicationActive from '../isApplicationActive';
 
 export class Application extends Component {
   state = {
     active: false,
   }
 
-  render({ application }) {
+  componentDidMount() {
+    const { application } = this.props;
+    if (isApplicationActive(application)) {
+      this.setState({
+        active: true,
+      });
+    }
+  }
+
+  render({ application }, { active }) {
     const { name, url, icon } = application;
+    const className = {
+      [style.Application__Active]: active,
+    };
     return (
-      <div className={style.Application}>
+      <div className={cx(style.Application, className)}>
         <a href={url}>
           <img src={icon} alt={name} />
           <span>{name}</span>
