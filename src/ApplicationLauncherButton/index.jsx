a2967ec32a66d2126c8774b498ca7bebcabad78b
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-07-05 13:33:17 +0200

    :lipstick: Base design for application launcher modal
    :sparkles: Modal is now closeable

--- src/ApplicationLauncherButton/index.jsx
+++ src/ApplicationLauncherButton/index.jsx
@@ -8,25 +8,25 @@ export class ApplicationLauncherButton extends Component {
     modalVisible: false,
   }
 
-  handleClick = () => {
-    this.showModal();
-  }
-
   showModal = () => {
     this.setState({ modalVisible: true });
+  }
+
+  hideModal = () => {
+    this.setState({ modalVisible: false });
   }
 
   render({ className, ...props }, { modalVisible }) {
     return (
       <div>
-        <button type="button" className={cx(style.ApplicationLauncherButton, className)} onClick={this.handleClick} {...props}>
+        <button type="button" className={cx(style.ApplicationLauncherButton, className)} onClick={this.showModal} {...props}>
           {/* https://material.io/tools/icons/?search=apps&icon=apps&style=baseline */}
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" />
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
         </button>
-        <ApplicationLauncherModal visible={modalVisible} />
+        <ApplicationLauncherModal visible={modalVisible} closeHandler={this.hideModal} />
       </div>
     );
   }
--- src/ApplicationLauncherModal/index.jsx
+++ src/ApplicationLauncherModal/index.jsx
@@ -2,13 +2,24 @@ import cx from 'classnames';
 import style from './style.module.scss';
 import { ApplicationContainer } from '../ApplicationContainer';
 
-export function ApplicationLauncherModal({ visible, ...props }) {
+export function ApplicationLauncherModal({ visible, closeHandler, ...props }) {
   const className = {
     [style.ApplicationLauncherModal__Visible]: visible,
   };
   return (
     <aside className={cx(style.ApplicationLauncherModal, className)} {...props}>
-      <ApplicationContainer grid {...props} />
+      <div className={style.ApplicationLauncherModal_Header}>
+        <button type="button" className={style.CloseButton} onClick={closeHandler}>
+          {/* https://material.io/tools/icons/?search=close&icon=close&style=baseline */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
+        </button>
+      </div>
+      <div className={style.ApplicationLauncherModal_Content}>
+        <ApplicationContainer grid {...props} />
+      </div>
     </aside>
   );
 }
