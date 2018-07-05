b8ee6cc6f02f909f09b85cbad1e71c20ac6a8468
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-07-05 14:18:41 +0200

    :lipstick: Launcher button stylings and allow to specify button icon color

--- src/ApplicationLauncherButton/index.jsx
+++ src/ApplicationLauncherButton/index.jsx
@@ -16,13 +16,13 @@ export class ApplicationLauncherButton extends Component {
     this.setState({ modalVisible: false });
   }
 
-  render({ className, ...props }, { modalVisible }) {
+  render({ className, color, ...props }, { modalVisible }) {
     return (
       <div>
         <button type="button" className={cx(style.ApplicationLauncherButton, className)} onClick={this.showModal} {...props}>
           {/* https://material.io/tools/icons/?search=apps&icon=apps&style=baseline */}
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" />
+            <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" fill={color} />
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
         </button>
