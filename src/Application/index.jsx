cec5c9c4a68c1110fc19ca220f54fa48f3156c0e
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-07-06 10:27:22 +0200

    :art: Make application component stateless

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,35 +1,19 @@
-import { Component } from 'preact';
 import cx from 'classnames';
 import style from './style.module.scss';
 import isApplicationActive from '../isApplicationActive';
 
-export class Application extends Component {
-  state = {
-    active: false,
-  }
-
-  componentDidMount() {
-    const { application } = this.props;
-    if (isApplicationActive(application)) {
-      this.setState({
-        active: true,
-      });
-    }
-  }
-
-  render({ application }, { active }) {
-    const { name, url, icon } = application;
-    const className = {
-      [style.Application__Active]: active,
-    };
-    return (
-      <div className={cx(style.Application, className)}>
-        <a href={url} title={name}>
-          <div className={style.Application_Content}>
-            <img src={icon} alt={name} />
-          </div>
-        </a>
-      </div>
-    );
-  }
+export function Application({ application }) {
+  const { name, url, icon } = application;
+  const classes = {
+    [style.Application__Active]: isApplicationActive(application),
+  };
+  return (
+    <div className={cx(style.Application, classes)}>
+      <a href={url} title={name}>
+        <div className={style.Application_Content}>
+          <img src={icon} alt={name} />
+        </div>
+      </a>
+    </div>
+  );
 }
