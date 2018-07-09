e760206fbbb91612a2caac2a1c6d964cd40c7061
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-07-09 11:14:55 +0200

    :lipstick: Button styling

--- src/ApplicationLauncherButton/index.jsx
+++ src/ApplicationLauncherButton/index.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'preact';
-import cx from 'classnames';
 import style from './style.module.scss';
 import { ApplicationLauncherModal } from '../ApplicationLauncherModal';
 
@@ -19,9 +18,9 @@ export class ApplicationLauncherButton extends Component {
   render(props, { modalVisible }) {
     return (
       <div className={style.ApplicationLauncherButtonWrapper} {...props}>
-        <button type="button" className={cx(style.ApplicationLauncherButton)} onClick={this.showModal}>
+        <button type="button" className={style.ApplicationLauncherButton} onClick={this.showModal}>
           {/* https://material.io/tools/icons/?search=apps&icon=apps&style=baseline */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <svg className={style.ApplicationLauncherButton_Icon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z" />
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
