568f7929e9c9f06700ec1788ad3af9b2040c139a
Arnaud Jabin <arnaud.jabin_ext@michelin.com>
2018-07-24 14:43:42 +0200

    :sparkles: Animate image appearance once loaded

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,21 +1,40 @@
+import { Component } from 'preact';
 import cx from 'classnames';
 import style from './style.module.scss';
 import { isUrlActiveForApplication } from '../isApplicationActive';
 import { PlaceholderImage } from './Placeholder/PlaceholderImage';
 
-export function Application({
-  application, className, activeClassName, url: currentUrl, onClick,
-}) {
-  const { name, url, icon } = application;
-  const classes = {
-    [activeClassName]: isUrlActiveForApplication(currentUrl, application),
-  };
-  return (
-    <li className={cx(style.Application, className, classes)}>
-      <a href={url} title={name} onClick={onClick}>
-        <PlaceholderImage className={style.PlaceholderImage} />
-        <img src={icon} alt={name} />
-      </a>
-    </li>
-  );
+export class Application extends Component {
+  state = {
+    loaded: false,
+  }
+
+  imageOnLoad() {
+    this.setState({
+      loaded: true,
+    });
+  }
+
+  render(
+    {
+      application, className, activeClassName, url: currentUrl, onClick,
+    },
+    {
+      loaded,
+    },
+  ) {
+    const { name, url, icon } = application;
+    const classes = {
+      [activeClassName]: isUrlActiveForApplication(currentUrl, application),
+      [style.Application__Loaded]: loaded,
+    };
+    return (
+      <li className={cx(style.Application, className, classes)}>
+        <a href={url} title={name} onClick={onClick}>
+          <PlaceholderImage className={style.PlaceholderImage} />
+          <img src={icon} alt={name} onLoad={() => this.imageOnLoad()} />
+        </a>
+      </li>
+    );
+  }
 }
